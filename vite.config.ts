25af63c643f89f2ca1aab7e896ba082d5177692f
kxf5555 <kxf5555@gmail.com>
2025-05-29 04:03:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,13 @@
-import { defineConfig } from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({plugins:[react()]});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),  // ★ 新增：@ 指向 src 文件夹
+    },
+  },
+})
